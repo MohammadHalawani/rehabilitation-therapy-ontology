2be0258df91ed51a89182a366370d8723fe18838
Mohammad Halawani <32101730+MohammadHalawani@users.noreply.github.com>
2023-03-10 18:25:03 +0000

    Delete rehabilitation-therapy-ontology directory

--- rehabilitation-therapy-ontology/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to rehabilitation-therapy-ontology
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- rehabilitation-therapy-ontology/test/rehabilitation_therapy_ontology/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns rehabilitation-therapy-ontology.core-test
-  (:require [clojure.test :refer :all]
-            [rehabilitation-therapy-ontology.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
